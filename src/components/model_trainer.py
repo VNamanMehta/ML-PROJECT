2568f7fb3c55b28980c81a4b36580dadb93dec25
V Naman Mehta <vnm.official.01@gmail.com>
2024-11-10 12:23:21 +0530

    hyper parameter tuning

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -42,7 +42,62 @@ class ModelTrainer:
                 "AdaBoostRegressor": AdaBoostRegressor(),
             }
 
-            model_report :dict = evaluate_model(X_train = X_train,Y_train = Y_train,X_test = X_test,Y_test = Y_test,models = models)
+            '''
+            HYPER PARAMETER TUNING
+
+            It is the process of selecting the optimal values for a machine learning model's hyperparameters.
+            These are settings that control the learning process of the model, 
+            rather than being learned from the data itself.
+
+            n_estimators = It represents the number of decision trees in the ensemble
+            Ensemble Learning: These algorithms create a "forest"(ensemble) of decision trees, each trained on a random subset of the data. 
+            n_estimators: Determines the size of this forest.
+            
+            criterion = It determines the function used to measure the quality of a split at each node of the tree
+
+            learning rate = The learning rate is a hyperparameter that controls the step size taken during gradient descent.
+            It determines how quickly the model's parameters are updated in response to the error gradient.
+
+            subsample = It controls the fraction of observations (rows) used to train each individual tree in an ensemble.
+
+            depth = It determines the maximum depth of the tree, which is the number of levels a tree can grow.
+
+            iterations = 
+
+            '''
+            params={
+                "Decision Tree": {
+                    'criterion':['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
+                },
+                "Random Forest":{
+                    'n_estimators': [8,16,32,64,128,256]
+                },
+                "Gradient Boosting":{
+                    'learning_rate':[.1,.01,.05,.001],
+                    'subsample':[0.6,0.7,0.75,0.8,0.85,0.9],
+                    'n_estimators': [8,16,32,64,128,256]
+                },
+                "Linear Regression":{},
+                "K-Neighors Regressor":{
+                    "n_neighbors": [3,5,7,9,11]
+                },
+                "XGBRegressor":{
+                    'learning_rate':[.1,.01,.05,.001],
+                    'n_estimators': [8,16,32,64,128,256]
+                },
+                "CatBoostRegressor":{
+                    'depth': [6,8,10],
+                    'learning_rate': [0.01, 0.05, 0.1],
+                    'iterations': [30, 50, 100]
+                },
+                "AdaBoostRegressor":{
+                    'learning_rate':[.1,.01,0.5,.001],
+                    'n_estimators': [8,16,32,64,128,256]
+                }
+                
+            }
+
+            model_report :dict = evaluate_model(X_train = X_train,Y_train = Y_train,X_test = X_test,Y_test = Y_test,models = models, params = params)
 
             best_model_score = max(sorted(model_report.values()))
 
--- src/utils.py
+++ src/utils.py
@@ -9,6 +9,7 @@ import sys
 import numpy as np
 import pandas as pd
 from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
 from src.exception import CustomException
 import dill # dill is used to serialize and save an object
 
@@ -36,12 +37,19 @@ file_obj: The file object (obtained from open()) where the serialized data will 
     except Exception as e:
         raise CustomException(e, sys)
     
-def evaluate_model(X_train,Y_train,X_test,Y_test,models):
+def evaluate_model(X_train,Y_train,X_test,Y_test,models,params):
     try:
         report = {}
          
         for i in range(len(models)):
             model = list(models.values())[i]
+            
+            param = params[list(models.keys())[i]]
+
+            grid_search = GridSearchCV(model, param, cv=3) 
+            grid_search.fit(X_train,Y_train)
+
+            model.set_params(**grid_search.best_params_)
             model.fit(X_train,Y_train)
 
             y_train_pred = model.predict(X_train)
